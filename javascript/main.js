d3b1d07727cea4ea3abc67f8681a632a5e0c8fda
TetratedBy2 <59283818+TetratedBy2@users.noreply.github.com>
2023-08-04 23:04:12 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+import Decimal from "decimal.mjs";
+
+var game = {
+    number: new Decimal(1),
+    increase: new Decimal(2),
+}
+
+function increaseNumber() {
+    game.number = game.number.mul(game.increase.pow(1 / 20))
+}
+
+function updateText() {
+    document.getElementById("current_number").innerHTML = game.number
+}
+
+window.setInterval(function() {
+    increaseNumber();
+    updateText();
+}, 20)
