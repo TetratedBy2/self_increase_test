0636892f7962188aac3bd1f316c9131541800d92
TetratedBy2 <59283818+TetratedBy2@users.noreply.github.com>
2023-08-05 09:44:56 +0200

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -4,14 +4,22 @@ var game = {
 }
 
 function increaseNumber() {
-    game.number = game.number.mul(game.increase.pow(1 / 20))
+    game.number = game.number.mul(game.increase.pow(0.05))
 }
 
 function updateText() {
     document.getElementById("current_number").innerHTML = game.number
 }
 
+function formatNumber(input) {
+    let power = Decimal.floor(Decimal.log(input))
+    let mantissa = input / Decimal.pow(10, power)
+    if (power < 3) return input
+    return mantissa + "e" + power
+}
+
 window.setInterval(function() {
     increaseNumber();
     updateText();
+    formatNumber();
 }, 20)
