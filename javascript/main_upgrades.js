8c553bdf775eab3a7fc515a8c8e5a088268c3191
TetratedBy2 <59283818+TetratedBy2@users.noreply.github.com>
2023-08-06 17:44:05 +0200

    Update main_upgrades.js

--- javascript/main_upgrades.js
+++ javascript/main_upgrades.js
@@ -1,5 +1,5 @@
 function buyMainUpg1() {
-    if (game.number >= game.mainUpg1Cost) {
+    if (OmegaNum(game.number) >= OmegaNum(game.mainUpg1Cost)) {
         game.number = game.number.divide(game.mainUpg1Cost)
         game.mainUpg1Cost = game.mainUpg1Cost.pow(2)
         game.mainUpg1 = game.mainUpg1.add(1)
